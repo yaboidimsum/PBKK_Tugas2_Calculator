3c7486880c595e013d2162b5ca603edc0aed4cd1
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-09-04 09:03:57 +0700

    add:tugas_2

---
+++ CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNilai1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNilai2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbHasil = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMines = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbNilai1
+            // 
+            this.tbNilai1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNilai1.Location = new System.Drawing.Point(164, 87);
+            this.tbNilai1.Name = "tbNilai1";
+            this.tbNilai1.Size = new System.Drawing.Size(299, 26);
+            this.tbNilai1.TabIndex = 0;
+            this.tbNilai1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("DM Mono", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(41, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nilai 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("DM Mono", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(41, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nilai 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbNilai2
+            // 
+            this.tbNilai2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNilai2.Location = new System.Drawing.Point(164, 158);
+            this.tbNilai2.Name = "tbNilai2";
+            this.tbNilai2.Size = new System.Drawing.Size(299, 26);
+            this.tbNilai2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("DM Mono", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(67, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hasil:";
+            // 
+            // lbHasil
+            // 
+            this.lbHasil.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbHasil.Location = new System.Drawing.Point(167, 236);
+            this.lbHasil.Name = "lbHasil";
+            this.lbHasil.Size = new System.Drawing.Size(299, 26);
+            this.lbHasil.TabIndex = 5;
+            this.lbHasil.TextChanged += new System.EventHandler(this.lbHasil_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnMultiply, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnDivide, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSquare, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnDelete, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnMines, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(79, 335);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(384, 100);
+            this.tableLayoutPanel1.TabIndex = 6;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Font = new System.Drawing.Font("DM Mono Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(121, 44);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMines
+            // 
+            this.btnMines.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMines.Font = new System.Drawing.Font("DM Mono Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMines.Location = new System.Drawing.Point(3, 53);
+            this.btnMines.Name = "btnMines";
+            this.btnMines.Size = new System.Drawing.Size(121, 44);
+            this.btnMines.TabIndex = 1;
+            this.btnMines.Text = "-";
+            this.btnMines.UseVisualStyleBackColor = true;
+            this.btnMines.Click += new System.EventHandler(this.btnMines_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMultiply.Font = new System.Drawing.Font("DM Mono Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(130, 3);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(122, 44);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDivide.Font = new System.Drawing.Font("DM Mono Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(130, 53);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(122, 44);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSquare.Font = new System.Drawing.Font("DM Mono Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSquare.Location = new System.Drawing.Point(258, 3);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(123, 44);
+            this.btnSquare.TabIndex = 4;
+            this.btnSquare.Text = "**";
+            this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Font = new System.Drawing.Font("DM Mono Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(258, 53);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 44);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "C";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(524, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lbHasil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbNilai2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNilai1);
+            this.MinimumSize = new System.Drawing.Size(546, 506);
+            this.Name = "Form1";
+            this.Text = "Calculator App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNilai1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNilai2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lbHasil;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnMines;
+    }
+}
+
---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the strings from the TextBoxes
+            string Nilai1 = tbNilai1.Text;
+            string Nilai2 = tbNilai2.Text;
+
+            // Declare integer variables to store the parsed values
+            int parsedNilai1;
+            int parsedNilai2;
+
+            // Use int.TryParse to safely attempt the conversion
+            if (int.TryParse(Nilai1, out parsedNilai1) && int.TryParse(Nilai2, out parsedNilai2))
+            {
+                // Conversion succeeded, and parsedNilai1 and parsedNilai2 now hold the integer values
+                // You can use them in your code here
+                // For example, you can add them together and display the result:
+                int sum = parsedNilai1 + parsedNilai2;
+                lbHasil.Text = sum.ToString();
+            }
+            else
+            {
+                // Conversion failed, handle the error here (e.g., display an error message)
+                lbHasil.Text = "Invalid input. Please enter valid integer values.";
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Get the strings from the TextBoxes
+            string Nilai1 = tbNilai1.Text;
+            string Nilai2 = tbNilai2.Text;
+
+            // Declare integer variables to store the parsed values
+            int parsedNilai1;
+            int parsedNilai2;
+
+            // Use int.TryParse to safely attempt the conversion
+            if (int.TryParse(Nilai1, out parsedNilai1) && int.TryParse(Nilai2, out parsedNilai2))
+            {
+                // Conversion succeeded, and parsedNilai1 and parsedNilai2 now hold the integer values
+                // You can use them in your code here
+                // For example, you can divide them together and display the result:
+                int divide = parsedNilai1 / parsedNilai2;
+                lbHasil.Text = divide.ToString();
+            }
+            else
+            {
+                // Conversion failed, handle the error here (e.g., display an error message)
+                lbHasil.Text = "Invalid input. Please enter valid integer values.";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lbHasil.Text = "";  
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbHasil_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMines_Click(object sender, EventArgs e)
+        {
+            // Get the strings from the TextBoxes
+            string Nilai1 = tbNilai1.Text;
+            string Nilai2 = tbNilai2.Text;
+
+            // Declare integer variables to store the parsed values
+            int parsedNilai1;
+            int parsedNilai2;
+
+            // Use int.TryParse to safely attempt the conversion
+            if (int.TryParse(Nilai1, out parsedNilai1) && int.TryParse(Nilai2, out parsedNilai2))
+            {
+                // Conversion succeeded, and parsedNilai1 and parsedNilai2 now hold the integer values
+                // You can use them in your code here
+                // For example, you can minus them together and display the result:
+                int minus = parsedNilai1 - parsedNilai2;
+                lbHasil.Text = minus.ToString();
+            }
+            else
+            {
+                // Conversion failed, handle the error here (e.g., display an error message)
+                lbHasil.Text = "Invalid input. Please enter valid integer values.";
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            // Get the strings from the TextBoxes
+            string Nilai1 = tbNilai1.Text;
+            string Nilai2 = tbNilai2.Text;
+
+            // Declare integer variables to store the parsed values
+            int parsedNilai1;
+            int parsedNilai2;
+
+            // Use int.TryParse to safely attempt the conversion
+            if (int.TryParse(Nilai1, out parsedNilai1) && int.TryParse(Nilai2, out parsedNilai2))
+            {
+                // Conversion succeeded, and parsedNilai1 and parsedNilai2 now hold the integer values
+                // You can use them in your code here
+                // For example, you can multiply them together and display the result:
+                int multiply = parsedNilai1 * parsedNilai2;
+                lbHasil.Text = multiply.ToString();
+            }
+            else
+            {
+                // Conversion failed, handle the error here (e.g., display an error message)
+                lbHasil.Text = "Invalid input. Please enter valid integer values.";
+            }
+        }
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            // Get the strings from the TextBoxes
+            string Nilai1 = tbNilai1.Text;
+            string Nilai2 = tbNilai2.Text;
+
+            // Declare integer variables to store the parsed values
+            int parsedNilai1;
+            int parsedNilai2;
+
+            // Use int.TryParse to safely attempt the conversion
+            if (int.TryParse(Nilai1, out parsedNilai1) && int.TryParse(Nilai2, out parsedNilai2))
+            {
+                // Conversion succeeded, and parsedNilai1 and parsedNilai2 now hold the integer values
+                // Calculate Nilai1 to the power of Nilai2
+                double result = Math.Pow(parsedNilai1, parsedNilai2);
+
+                // Convert the result back to an integer if needed
+                int square = (int)result;
+
+                lbHasil.Text = square.ToString();
+            }
+            else
+            {
+                // Conversion failed, handle the error here (e.g., display an error message)
+                lbHasil.Text = "Invalid input. Please enter valid integer values.";
+            }
+        }
+    }
+}
